7d3bc35800898245963b4022879fac96d7e98e89
OcteauJeremy <octeau.jeremy@gmail.com>
2018-02-23 00:44:57 -0800

    Removed merge

--- webapp/src/app/market-module/price-chart/price-chart.component.ts
+++ webapp/src/app/market-module/price-chart/price-chart.component.ts
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
-// import { Chart } from 'chart.js';
-=======
 import { Component, Input, OnInit } from '@angular/core';
->>>>>>> 1c0c6481523fe35a501e8cf93569481f8694c381
 
 @Component({
   selector: 'price-chart',
@@ -24,16 +19,5 @@ export class PriceChartComponent implements OnInit {
       this.datas.push(tx.price);
     }
     this.datas.push(this.card.price);
-<<<<<<< HEAD
-    console.log(this.datas);
-
-    var ctx = document.getElementById("chart-" + this.card.id);
-    console.log(ctx);
-    // var myChart = new Chart(ctx, {
-    //   type: 'line',
-    //   data: this.datas
-    // });
-=======
->>>>>>> 1c0c6481523fe35a501e8cf93569481f8694c381
   }
 }
