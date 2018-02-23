d1ddc08c0d55c0bb7d846499e0552deb86bdb5e5
OcteauJeremy <octeau.jeremy@gmail.com>
2018-02-22 22:15:47 -0800

    transaction component

---
+++ webapp/src/app/market-module/price-chart/price-chart.component.ts
@@ -0,0 +1,32 @@
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'price-chart',
+  templateUrl: './price-chart.component.html',
+  styleUrls: ['./price-chart.component.css']
+})
+export class PriceChartComponent implements OnInit {
+
+  @Input("card")  card;
+
+  public datas = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    for (let tx of this.card.transactions) {
+      this.datas.push(tx.price);
+    }
+    this.datas.push(this.card.price);
+    console.log(this.datas);
+
+    var ctx = document.getElementById("chart-" + this.card.id);
+    console.log(ctx);
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: this.datas
+    });
+  }
+}
---
+++ webapp/src/app/market-module/transactions/transactions.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'transactions',
+  templateUrl: './transactions.component.html',
+  styleUrls: ['./transactions.component.css']
+})
+export class TransactionsComponent implements OnInit {
+
+  @Input("transactions") transactions;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('transactions', this.transactions)
+  }
+
+}
