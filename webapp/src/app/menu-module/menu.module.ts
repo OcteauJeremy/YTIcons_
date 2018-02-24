d79a20401843582a9627eef4a6e8d37b74aae76e
Nyrii <nyrandone.noboud-inpeng@epitech.eu>
2018-02-24 01:04:17 -0800

    Responsive home

--- webapp/src/app/menu-module/menu.module.ts
+++ webapp/src/app/menu-module/menu.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { RouterModule } from '@angular/router';
-import { TopbarModule } from '../topbar-module/topbar.module';
-import {UserInfoComponent} from '../topbar-module/user-info/user-info.component';
-import {NotificationsComponent} from '../topbar-module/notifications/notifications.component';
-import {CurrencySelectorComponent} from '../topbar-module/currency-selector/currency-selector.component';
-import {AngularFontAwesomeModule} from 'angular-font-awesome';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { CurrencySelectorComponent } from './currency-selector/currency-selector.component';
 
 @NgModule({
   imports: [
