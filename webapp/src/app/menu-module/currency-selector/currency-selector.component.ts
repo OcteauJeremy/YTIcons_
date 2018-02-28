abe9547da9567d2cf6ccae006dfcf682995975a2
Nyrii <nyrandone.noboud-inpeng@epitech.eu>
2018-02-27 16:34:26 -0800

    FAQ MetaMask video responsive

--- webapp/src/app/menu-module/currency-selector/currency-selector.component.ts
+++ webapp/src/app/menu-module/currency-selector/currency-selector.component.ts
@@ -1,34 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
 import { UserService } from '../../services/user.service';
 import { CurrencyService } from '../../services/currency.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'currency-selector',
   templateUrl: './currency-selector.component.html',
   styleUrls: ['./currency-selector.component.css']
 })
-export class CurrencySelectorComponent implements OnInit {
+export class CurrencySelectorComponent implements OnInit, OnDestroy {
 
   public currency = "ETH";
   public currentUser;
+  private subscriptions: Subscription = new Subscription();
 
   constructor(private authenticationService: AuthenticationService, private currencyService: CurrencyService, private userService: UserService) {
 
-    this.authenticationService.currentUserChange.subscribe((user) => {
-      this.currentUser = user;
-      this.currency = user.currency;
-    });
+    this.subscriptions.add(
+      this.authenticationService.currentUserChange.subscribe((user) => {
+        this.currentUser = user;
+        this.currency = user.currency;
+      })
+    );
 
-    this.currencyService.currentCurrencyChange.subscribe((currency) => {
-      this.currency = currency;
-      this.currentUser = this.authenticationService.getLocalUser();
-      if (this.currentUser) {
-        this.userService.modifyUser(this.currentUser).subscribe(res => {
-          this.authenticationService.setCurrentUser(res);
-        });
-      }
-    });
+    this.subscriptions.add(
+      this.currencyService.currentCurrencyChange.subscribe((currency) => {
+        this.currency = currency;
+        this.currentUser = this.authenticationService.getLocalUser();
+        if (this.currentUser) {
+          this.subscriptions.add(
+            this.userService.modifyUser(this.currentUser).subscribe(res => {
+              this.authenticationService.setCurrentUser(res);
+            })
+          );
+        }
+      })
+    );
 
     this.currentUser = this.authenticationService.currentUser;
     if (this.currentUser) {
@@ -43,6 +51,10 @@ export class CurrencySelectorComponent implements OnInit {
 
   setCurrency(currency) {
     this.currencyService.setCurrency(currency);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 
 }
