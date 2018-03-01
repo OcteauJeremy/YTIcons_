106e4ea198c901fc8d09240c1bacae39750a0ae7
albanbin <alban.combaud@epitech.eu>
2018-03-01 15:18:43 -0800

    input

--- webapp/src/app/market-module/card/card.component.ts
+++ webapp/src/app/market-module/card/card.component.ts
@@ -17,7 +17,7 @@ export class CardComponent implements OnInit {
     _id: ""
   };
 
-  public obj: any;
+  // public obj: any;
 
   constructor(private authenticationService: AuthenticationService, public cs: CardService,
               private router: Router) {
@@ -29,36 +29,36 @@ export class CardComponent implements OnInit {
   }
 
 
-  hoverEffect(x, y){
-
-    let eWidth = this.obj.clientWidth;
-    let eHeight = this.obj.clientHeight;
-    let xPos = (x - (eWidth / 2)) / 30;
-    let yPos = (y - (eHeight / 2)) / 30;
-
-    var rotateX = Math.round(+ (yPos * 0.6));
-    var rotateY = Math.round(- (xPos * 1));
-
-    this.obj.style.transition = 'all 0s linear';
-    this.obj.style.transform = 'scale(1.0) rotateX(' + rotateX + 'deg) rotateY(' + rotateY + 'deg)';
-    this.obj.style.zIndex = '99';
-  }
+  // hoverEffect(x, y){
+  //
+  //   let eWidth = this.obj.clientWidth;
+  //   let eHeight = this.obj.clientHeight;
+  //   let xPos = (x - (eWidth / 2)) / 30;
+  //   let yPos = (y - (eHeight / 2)) / 30;
+  //
+  //   var rotateX = Math.round(+ (yPos * 0.6));
+  //   var rotateY = Math.round(- (xPos * 1));
+  //
+  //   this.obj.style.transition = 'all 0s linear';
+  //   this.obj.style.transform = 'scale(1.0) rotateX(' + rotateX + 'deg) rotateY(' + rotateY + 'deg)';
+  //   this.obj.style.zIndex = '99';
+  // }
 
   ngOnInit() {
-    this.obj = document.getElementById(this.card._id);
-
-    this.obj.onmousemove = function(event){
-      var relX = event.pageX - this.obj.offsetLeft;
-      var relY = event.pageY - this.obj.offsetTop;
-
-      this.hoverEffect(relX, relY);
-    };
-
-    this.obj.onmouseleave = function(){
-      this.obj.style.transition = 'all 0.8s ease-in-out';
-      this.obj.style.transform = 'rotate(0deg)';
-      this.obj.style.zIndex = '0';
-    };
+    // this.obj = document.getElementById(this.card._id);
+    //
+    // this.obj.onmousemove = function(event){
+    //   var relX = event.pageX - this.obj.offsetLeft;
+    //   var relY = event.pageY - this.obj.offsetTop;
+    //
+    //   this.hoverEffect(relX, relY);
+    // };
+    //
+    // this.obj.onmouseleave = function(){
+    //   this.obj.style.transition = 'all 0.8s ease-in-out';
+    //   this.obj.style.transform = 'rotate(0deg)';
+    //   this.obj.style.zIndex = '0';
+    // };
 
   }
 }
