c163749a194bb7705f995d1223e43b875e07e5bd
OcteauJeremy <octeau.jeremy@gmail.com>
2018-03-06 14:09:30 -0800

    ajout protocol configurable dans les environnements

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
+
+const protocol = "http://";
+
 export const environment = {
   production: true,
-  apiUrl: 'http://yticons.co:3000',
+  apiUrl: protocol + 'yticons.co:3000',
   tokenAddress: '0x0551ddc4460e09cc30bec37ef73a8b9739dc179a',
   recaptchaPublic: '6Ld_fkoUAAAAAMD2oHdp6l6Sl3lICiigExvs5KDm',
   recaptchaPrivate: '6Ld_fkoUAAAAAG89zvHX8vV5wVWK8S_8O1GDCa4M'
--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -3,9 +3,11 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+const protocol = "http://";
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: protocol + 'localhost:3000',
   tokenAddress: '0x10207693450fa18527f73d71d52f6c016d8353fa',
   recaptchaPublic: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
   recaptchaPrivate: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
