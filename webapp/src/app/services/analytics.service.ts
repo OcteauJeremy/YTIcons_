c35a597bb40c443f0f8e9f88a051a2621b15344e
nekfeu <kevin.empociello@epitech.eu>
2018-03-06 00:21:36 -0800

    Clean

--- webapp/src/app/services/analytics.service.ts
+++ webapp/src/app/services/analytics.service.ts
@@ -6,7 +6,6 @@ export class AnalyticsService {
   constructor() { }
 
   sendEvent(eventCategory: string, eventAction: string) {
-    console.log('coucou');
     (<any>window).ga('send', 'event', { eventCategory: eventCategory, eventAction: eventAction});
   }
 }
