d60485331f7685ca904a2b3df89d65b692c8bd52
OcteauJeremy <octeau.jeremy@gmail.com>
2018-03-06 01:00:13 -0800

    New SC prod

--- api/configs/urls.js
+++ api/configs/urls.js
@@ -2,7 +2,7 @@
 
 if (process.env.ENVIRONMENT == "prod") {
     module.exports = {
-        tokenAddress: "0xc1d4a8b2639ddc2ab23f882b30f37904ad32f908",
+        tokenAddress: "0x7218c57567fac42ec721b364588b7f72a59fa870",
         webserver: "http://163.172.142.44/",
         websocket: "wss://ropsten.infura.io/ws",
         mongodb: "mongodb://yticons:SPy55g47Q7bTstN6@yticons-mongo:27017/yticons"
--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'http://163.172.142.44:3000',
-  tokenAddress: '0xc1d4a8b2639ddc2ab23f882b30f37904ad32f908',
+  tokenAddress: '0x7218c57567fac42ec721b364588b7f72a59fa870',
   recaptchaPublic: '6Ld_fkoUAAAAAMD2oHdp6l6Sl3lICiigExvs5KDm',
   recaptchaPrivate: '6Ld_fkoUAAAAAG89zvHX8vV5wVWK8S_8O1GDCa4M'
 };
