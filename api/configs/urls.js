d1246f31d4ba50e30ccfb717f9ae432e2fbe3218
OcteauJeremy <octeau.jeremy@gmail.com>
2018-03-06 03:34:53 -0800

    new SC prod

--- api/configs/urls.js
+++ api/configs/urls.js
@@ -2,7 +2,7 @@
 
 if (process.env.ENVIRONMENT == "prod") {
     module.exports = {
-        tokenAddress: "0x7218c57567fac42ec721b364588b7f72a59fa870",
+        tokenAddress: "0x0551ddc4460e09cc30bec37ef73a8b9739dc179a",
         webserver: "http://yticons.co",
         websocket: "wss://ropsten.infura.io/ws",
         mongodb: "mongodb://yticons:SPy55g47Q7bTstN6@yticons-mongo:27017/yticons"
--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'http://yticons.co:3000',
-  tokenAddress: '0x7218c57567fac42ec721b364588b7f72a59fa870',
+  tokenAddress: '0x0551ddc4460e09cc30bec37ef73a8b9739dc179a',
   recaptchaPublic: '6Ld_fkoUAAAAAMD2oHdp6l6Sl3lICiigExvs5KDm',
   recaptchaPrivate: '6Ld_fkoUAAAAAG89zvHX8vV5wVWK8S_8O1GDCa4M'
 };
