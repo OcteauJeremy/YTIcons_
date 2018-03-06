98f7e99f1398bcbba6245e6210c4e159c7a15a29
OcteauJeremy <octeau.jeremy@gmail.com>
2018-03-06 03:29:09 -0800

    Correction bug admin page + creation ID card SC cote backend + new SC dev

--- api/models/card.model.js
+++ api/models/card.model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CardSchema = mongoose.Schema({
-    id: {type: Number},
+    id: Number,
     name: String,
     image: String,
     nationality: {type: Schema.Types.ObjectId, ref: "Nationality"},
