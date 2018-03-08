62012f814fc3e4f4a7cef79e64443b3c377c3290
OcteauJeremy <octeau.jeremy@gmail.com>
2018-03-07 16:19:26 -0800

    Modification port between prod & dev

--- api/configs/urls.js
+++ api/configs/urls.js
@@ -3,6 +3,7 @@
 if (process.env.ENVIRONMENT == "prod") {
     var protocol = "https://";
     module.exports = {
+        port: "8443",
         tokenAddress: "0x0551ddc4460e09cc30bec37ef73a8b9739dc179a",
         secureSocket: protocol == "https://" ? true : false,
         webserver: protocol + "yticons.co",
@@ -12,6 +13,7 @@ if (process.env.ENVIRONMENT == "prod") {
 } else {
     var protocol = "http://";
     module.exports = {
+        port: "6969",
         tokenAddress: "0x84e6c6de5b5b98bf28763d307ed1d6ec4b56d383",
         secureSocket: protocol == "https://" ? true : false,
         webserver: "http://localhost:4200",
