186839ed44eb800e7c255cc168ba9cfa09fe6185
OcteauJeremy <octeau.jeremy@gmail.com>
2018-03-08 23:48:28 -0800

    new SC prod

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   production: true,
   apiUrl: protocol + 'yticons.co:8443',
   secureSocket: protocol == "https://",
-  tokenAddress: '0x302e29c249b4b297a45a4be25c25e14e46196fd5',
+  tokenAddress: '0xa71d4d37e8a945494453fb6e37aba58c482d28d3',
   recaptchaPublic: '6Ld_fkoUAAAAAMD2oHdp6l6Sl3lICiigExvs5KDm',
   recaptchaPrivate: '6Ld_fkoUAAAAAG89zvHX8vV5wVWK8S_8O1GDCa4M'
 };
