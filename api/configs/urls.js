b33bee942bb36dda3ff512c769f26ecebd275691
OcteauJeremy <octeau.jeremy@gmail.com>
2018-03-17 18:34:46 -0700

    New password prod

--- api/configs/urls.js
+++ api/configs/urls.js
@@ -4,12 +4,12 @@ if (process.env.ENVIRONMENT == "prod") {
     var protocol = "https://";
     module.exports = {
         port: "8443",
-        tokenAddress: "0xe318ccb57a5e7f5c97dbe56dd962dbc8fd8a3416",
+        tokenAddress: "0x3e5a743a07cdf7a6406d3e62d4172c8168251a3a",
         secureSocket: protocol == "https://" ? true : false,
         webserver: protocol + "yticons.co",
         // websocket: "wss://ropsten.infura.io/ws",
         websocket: "ws://yticons-parity:8546",
-        mongodb: "mongodb://yticons:SPy55g47Q7bTstN6@yticons-mongo:27017/yticons",
+        mongodb: "mongodb://yticons:nVusaLU6rc3thMWuQ4x8uzbdNS7LPwNqmSVzawdqZ56KJDdMP4TyFmZ6tBMB3SjT@yticons-mongo:27017/yticons",
         recaptchaPrivate: '6Ld_fkoUAAAAAG89zvHX8vV5wVWK8S_8O1GDCa4M'
     };
 } else {
--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   production: true,
   apiUrl: protocol + 'yticons.co:8443',
   secureSocket: protocol == "https://",
-  tokenAddress: '0xe318ccb57a5e7f5c97dbe56dd962dbc8fd8a3416',
+  tokenAddress: '0x3e5a743a07cdf7a6406d3e62d4172c8168251a3a',
   recaptchaPublic: '6Ld_fkoUAAAAAMD2oHdp6l6Sl3lICiigExvs5KDm',
   recaptchaPrivate: '6Ld_fkoUAAAAAG89zvHX8vV5wVWK8S_8O1GDCa4M'
 };
