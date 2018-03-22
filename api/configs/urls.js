d05ce9f6ed1ace33a469c294d8430349950f9c5b
OcteauJeremy <octeau.jeremy@gmail.com>
2018-03-22 15:35:23 -0400

    New sc prod

--- api/configs/urls.js
+++ api/configs/urls.js
@@ -4,7 +4,7 @@ if (process.env.ENVIRONMENT == "prod") {
     var protocol = "https://";
     module.exports = {
         port: "8443",
-        tokenAddress: "0xCe14a14E8D72cD44a53e7496307D5c687b9067f0",
+        tokenAddress: "0xAd3924b97db6D308B80cF43EbD682cd28C243Edb",
         secureSocket: protocol == "https://" ? true : false,
         webserver: protocol + "yticons.co",
         // websocket: "wss://ropsten.infura.io/ws",
--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   production: true,
   apiUrl: protocol + 'yticons.co:8443',
   secureSocket: protocol == "https://",
-  tokenAddress: '0xCe14a14E8D72cD44a53e7496307D5c687b9067f0',
+  tokenAddress: '0xAd3924b97db6D308B80cF43EbD682cd28C243Edb',
   recaptchaPublic: '6Ld_fkoUAAAAAMD2oHdp6l6Sl3lICiigExvs5KDm',
   recaptchaPrivate: '6Ld_fkoUAAAAAG89zvHX8vV5wVWK8S_8O1GDCa4M'
 };
