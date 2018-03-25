356f5875a49c7e763491b0fa4089c0ecce6b67cc
OcteauJeremy <octeau.jeremy@gmail.com>
2018-03-25 14:02:52 -0400

    Debug

--- api/controllers/web3.controller.js
+++ api/controllers/web3.controller.js
@@ -142,6 +142,7 @@ var getPastEventsSold = function () {
     tokenContract.getPastEvents('YTIconSold', {
         fromBlock: fromBlockSold
     }, function (error, events) {
+        console.log('getPastEventsSold', fromBlockSold);
 
         for (var i = 0; i < events.length; i++) {
           soldYTIcon(events[i].returnValues, events[i]);
