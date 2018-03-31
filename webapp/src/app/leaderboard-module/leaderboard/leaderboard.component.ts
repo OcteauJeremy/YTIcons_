d20e439d19a7049842824d72c68e4c45cbe6bdb8
albanbin <alban.combaud@epitech.eu>
2018-03-30 17:33:41 -0700

    countdown

--- webapp/src/app/leaderboard-module/leaderboard/leaderboard.component.ts
+++ webapp/src/app/leaderboard-module/leaderboard/leaderboard.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import { ManagerService } from "../../services/manager.service";
-import { LeaderboardService } from "../../services/leaderboard.service";
-import { Subscription } from "rxjs/Subscription";
-import { AuthenticationService } from "../../services/authentication.service";
-import { ToastsManager } from 'ng2-toastr';
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {ManagerService} from "../../services/manager.service";
+import {LeaderboardService} from "../../services/leaderboard.service";
+import {Subscription} from "rxjs/Subscription";
+import {AuthenticationService} from "../../services/authentication.service";
+import {ToastsManager} from 'ng2-toastr';
 import {CardService} from "../../services/card.service";
 import {RoundPipe} from "../../pipes/round.pipe";
 
@@ -12,14 +12,14 @@ import {RoundPipe} from "../../pipes/round.pipe";
   templateUrl: './leaderboard.component.html',
   styleUrls: ['./leaderboard.component.css']
 })
-export class LeaderboardComponent implements OnInit, OnDestroy {
+export class LeaderboardComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor(private roundPipe: RoundPipe, private ms: ManagerService,private ls: LeaderboardService, private as: AuthenticationService, private toastr: ToastsManager,private cs: CardService) {
+  constructor(private roundPipe: RoundPipe, private ms: ManagerService, private ls: LeaderboardService, private as: AuthenticationService, private toastr: ToastsManager, private cs: CardService) {
 
   }
 
   public leaderboard: any;
-  public  ranking;
+  public ranking;
   private subscriptions: Subscription = new Subscription();
   public currentUser: any;
   public isLoading: boolean = true;
@@ -35,7 +35,7 @@ export class LeaderboardComponent implements OnInit, OnDestroy {
       utilityTmp = parseInt(res);
       return utilityTmp;
 
-    }).then( res => {
+    }).then(res => {
       _self.cs.getSmartContractFund().then(res => {
         utilityTmp += parseInt(res) / 5;
         _self.utilityFund = this.roundPipe.transform(this.cs.getFromWei(utilityTmp.toString()));
@@ -50,8 +50,7 @@ export class LeaderboardComponent implements OnInit, OnDestroy {
     // });
 
 
-
-    this.subscriptions.add(toastr.onClickToast().subscribe( toast => {
+    this.subscriptions.add(toastr.onClickToast().subscribe(toast => {
       if (toast.timeoutId) {
         clearTimeout(toast.timeoutId);
       }
@@ -65,9 +64,56 @@ export class LeaderboardComponent implements OnInit, OnDestroy {
         _self.isLoading = false;
       }
     }, error => {
-      toastr.error( 'Can\'t get the current leaderboard', 'Leaderboard');
+      toastr.error('Can\'t get the current leaderboard', 'Leaderboard');
       _self.isLoading = false;
     }));
+  }
+
+  getTimeRemaining(endtime) {
+    let _d = new Date();
+    var t = Date.parse(endtime) - Date.parse(_d.toString());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  initializeClock(id, endtime) {
+    let _self = this;
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+      var t = _self.getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days.toString();
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+
+
+  ngAfterViewInit() {
+    let deadline = new Date(Date.UTC(2018, 3, 15, 11, 59, 59));
+    this.initializeClock('clockdiv', deadline);
   }
 
   ngOnDestroy() {
